9422f09aebc3d5ec59940a9f44fa1e93f4638373
Elenalvarez <elenalvarezsanchez@gmail.com>
2023-11-17 12:30:45 +0100

    Menú de pausa habilitado con el esc

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,23 @@
 extends Control
 
+var jugador 
+
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/room.tscn")
+	if !get_tree().paused:
+		get_tree().change_scene_to_file("res://scenes/room.tscn")
+		change_visibility()
+		await get_tree().create_timer(3).timeout
+		jugador = get_parent().get_parent().get_node("Player")
+	else:
+		change_visibility()
+		get_tree().paused = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+	$OptionMenu.visible = true
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+func change_visibility():
+	visible = !visible
--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -7,9 +7,9 @@ func _on_h_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(0, linear_to_db(value))
 
 func _on_sensitivity_value_changed(value):
+	#get_parent().jugador.change_sensitivity(value)
 	pass
 
 func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
-
+	visible= false
 
