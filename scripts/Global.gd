48999b2bcd4ed546464e9142a56eb94f582093e2
Elenalvarez <elenalvarezsanchez@gmail.com>
2023-11-29 18:01:20 +0100

    Bascula añadida a la room

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -2,3 +2,5 @@ extends Node
 
 var piano= false
 var cajonSupDcha= false
+var corazon= false
+var bascula= false
--- scripts/bascula.gd
+++ scripts/bascula.gd
@@ -1,37 +1,17 @@
-extends Node2D
+extends Control
 
-var texto=42 
+var texto="05,74"
 # Called when the node enters the scene tree for the first time.
 
 
 func _ready():
-	if Corazon.corazon:
+	if GLOBAL.corazon:
 		get_node("10x10CmCorazonDeLaVirgen").visible=true
 		get_node("Peso").visible=true
 		get_node("Peso").text=str(texto)
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_salida_pressed():
 	get_node(".").visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
 
-
-func _on_menos_pressed():
-	texto-=1
-	get_node("Peso").text=str(texto)
-	solucion()
-
-func _on_mas_pressed():
-	texto+=1
-	get_node("Peso").text=str(texto)
-	solucion()
-func solucion():
-	if texto==51:
-		get_node(".").visible = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
